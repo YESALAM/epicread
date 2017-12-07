8d6975e6b2ef7e7327189a045484ff1ef08ffdc9
yesalam <itisalam@gmail.com>
2017-12-07 16:18:13 +0530

    fancy progress for ReadPdf

--- ReadPdf.py
+++ ReadPdf.py
@@ -2,7 +2,7 @@ import PyPDF2
 import re
 import os
 import db_conn
-
+import fancy
 
 
 
@@ -30,13 +30,20 @@ def saveEpic(file_path,district,cur):
     with open(file_path,'rb') as file:
         pdfReader = PyPDF2.PdfFileReader(file)
         nop = pdfReader.numPages
+        file_name = os.path.basename(os.path.normpath(file_path))
+        assembly_name = os.path.basename(os.path.dirname(file_path))
+        if nop > 0:
+            # Initial call to print 0% progress
+            fancy.printProgressBar(0, nop, prefix='Reading Pdf '+ assembly_name + '/'  +file_name, suffix='Complete', length=50)
+
+        i = 1
         for i in range(0, nop):
             cp = pdfReader.getPage(i)
             str = cp.extractText()
             for line in iter(str.splitlines()):
                 epic = extractEPIC(line)
                 if epic is not None:
-                    print(epic)
+                    #print(epic)
                     sql_check = "select * from `epic_data` where `epic_no`='" + epic + "' and `queried`=1"
                     cur.execute(sql_check)
                     try:
@@ -47,18 +54,21 @@ def saveEpic(file_path,district,cur):
 
                     except:
                         pass
-
+            fancy.printProgressBar(i+1, nop, prefix='Reading Pdf ' + assembly_name + '/' + file_name ,
+                                   suffix='Complete', length=50)
+            i = i+1
 
 def readPdfs(cur,db):
     pdf_path = os.path.join('data', 'pdfs')
     districts = os.listdir(pdf_path)
     cwd = os.getcwd()
+    print("Reading pdfs to extract EPIC no")
     for district in districts:
-        print(district.title())
+        #print(district.title())
         district_path = os.path.join(cwd, pdf_path, district.title())
         assemblies = os.listdir(district_path)
         for assembly in assemblies:
-            print(assembly.title())
+            #print(assembly.title())
             assembly_path = os.path.join(district_path, assembly.title())
             parts = os.listdir(assembly_path)
             for part in parts:
@@ -68,8 +78,27 @@ def readPdfs(cur,db):
                     db.commit()
                     deletePdf(file_path)
 
+
 def deletePdf(file_path):
     os.remove(file_path)
+
+def noOfFiles():
+    nof = 0
+    pdf_path = os.path.join('data', 'pdfs')
+    districts = os.listdir(pdf_path)
+    cwd = os.getcwd()
+    for district in districts:
+        district_path = os.path.join(cwd, pdf_path, district.title())
+        assemblies = os.listdir(district_path)
+        for assembly in assemblies:
+            assembly_path = os.path.join(district_path, assembly.title())
+            parts = os.listdir(assembly_path)
+            for part in parts:
+                file_path = os.path.join(assembly_path, part.title().lower())
+                if os.path.isfile(file_path):
+                    nof = nof + 1
+
+    return nof
 
 
 if __name__ == "__main__":
